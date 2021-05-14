22b0d48662ec60d8e420faf50a921e4598514db8
rolnor <rolnor-8@student.ltu.se>
2021-05-14 13:36:26 +0200

    hmm

--- dlabb3/dlabb3.cpp
+++ dlabb3/dlabb3.cpp
@@ -16,6 +16,7 @@ struct Point2d
 
 class treeNode
 {
+private:
     int key;
     int hashPointer;
     int height;
@@ -23,8 +24,12 @@ class treeNode
     treeNode* rightLeaf;
 
 public:
+    treeNode(int iKey, int iHashpointer, int iHeight);
+    treeNode();
+    ~treeNode();
     treeNode* getLeft() { return this->leftLeaf; };
     treeNode* getRight() { return this->rightLeaf; };
+    void insert(treeNode& newNode);
     void insertLeft(treeNode Left);
     void insertRight(treeNode Right);
     int getData(myDataType DataValue);
@@ -33,6 +38,40 @@ public:
 int main()
 {
     std::cout << "Hello World!\n";
+}
+
+treeNode::treeNode(int iKey, int iHashpointer, int iHeight)
+{
+    leftLeaf = nullptr;
+    rightLeaf = nullptr;
+    key = iKey;
+    hashPointer = iHashpointer;
+    height = iHeight;
+}
+
+treeNode::treeNode()
+{
+    leftLeaf = nullptr;
+    rightLeaf = nullptr;
+    key = -1;
+    hashPointer = -1;
+    height = -1;
+}
+
+treeNode::~treeNode()
+{
+    while (leftLeaf == nullptr || rightLeaf == nullptr)
+    {
+        if (leftLeaf != nullptr)
+            delete leftLeaf;
+        if (rightLeaf != nullptr)
+            delete rightLeaf;
+    }
+}
+
+void treeNode::insert(int iKey, int iHash)
+{
+    if()
 }
 
 void treeNode::insertLeft(treeNode Left)
